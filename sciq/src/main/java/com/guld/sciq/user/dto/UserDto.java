fad4fead3a9bca2ad35bbacab49d7d493fb91980
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-04-15 10:22:16 +0900

    refactor: user기본 DTO 리팩터링

--- sciq/src/main/java/com/guld/sciq/user/dto/UserDto.java
+++ sciq/src/main/java/com/guld/sciq/user/dto/UserDto.java
@@ -1,56 +1,23 @@
 package com.guld.sciq.user.dto;
 
-import com.guld.sciq.user.entity.UserRole;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import com.guld.sciq.user.entity.User;
-import com.guld.sciq.user.entity.UserPrefer;
 
-public class UserDto {
-
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class Request {
-        private String name;
-        private String email;
-        private String nickname;
-        private String password;
-        private String schoolName;
-        private UserPrefer prefer;
-        private UserRole userRole;
-
+public record UserDto(
+    Long id,
+    String email,
+    String userName,
+    String nickName,
+    String schoolName
+) {
+    public static UserDto from(User user) {
+        return new UserDto(
+            user.getId(),
+            user.getEmail(),
+            user.getUserName(),
+            user.getNickName(),
+            user.getSchoolName()
+        );
     }
-
-
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class Response {
-        private String name;
-        private String email;
-        private String nickname;
-        private String schoolName;
-        private String prefer;
-        private String role;
-
-        public static Response from(User user) {
-            return Response.builder()
-                    .name(user.getUserName())
-                    .email(user.getEmail())
-                    .nickname(user.getNickName())
-                    .schoolName(user.getSchoolName())
-                    .prefer(user.getPrefer().name())
-                    .role(user.getRole().name())
-                    .build();
-        }
-    }
-
 }
 
 
